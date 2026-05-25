fb12b2f5d17c3c4b46d25db5117d72d0ec297133
Ernest Acquah <ernestacquah086@gmail.com>
2026-05-25 05:36:48 +0000

    Started Little Lemon React assignment

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 Little Lemon Restaurant</p>
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Reservations</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <p>Follow us on social media</p>
+    </footer>
+  );
+}
+
+export default Footer;
