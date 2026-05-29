3747bab7ef7e29e545bcde13ebbc2c368f17b4fa
Ernest Acquah <ernestacquah086@gmail.com>
2026-05-29 14:24:18 +0000

    Added responsive CSS and fixed logo import

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
+import React from "react";
 import logo from "./assets/logo.png";
 
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="Little Lemon Logo" className="logo" />
-      <h1>Little Lemon</h1>
+      <img src={logo} alt="Little Lemon logo" className="logo" />
+      <h1>Little Lemon Restaurant</h1>
     </header>
   );
 }
